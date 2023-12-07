32c5e6c020e8ce8b826f86e0ded16248c2cc19d3
VitaliyV22 <vitanskyyv@gmail.com>
2023-12-07 11:16:53 -0600

    compeleted logic for calculator

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,21 @@
-
-it('should calculate the monthly rate correctly', function () {
+it('should calculate the monthly rate correctly', function() {
   // ...
+  const values = {
+    amount: 5000,
+    years: 10,
+    rate: 2.3
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('46.68');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 5000,
+    years: 10,
+    rate: 2.3
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('46.68');
 });
 
-/// etc
+
